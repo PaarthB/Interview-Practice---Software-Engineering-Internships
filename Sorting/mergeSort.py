877d06b3674718fd2b504688ff3a6d42b30deda3
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-19 11:30:07 +1100

    Add files via upload

---
+++ Sorting/mergeSort.py
@@ -0,0 +1,36 @@
+def merge_sort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        lefthalf = array[:mid]
+        righthalf = array[mid:]
+
+        print(lefthalf)
+        print(righthalf)
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+
+        # merging the left and right halves together.
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                array[k] = lefthalf[i]
+                i += 1
+            else:
+                array[k] = righthalf[j]
+                j += 1
+            k += 1
+
+        while i < len(lefthalf):
+            array[k] = lefthalf[i]
+            i += 1
+            k += 1
+
+        while j < len(righthalf):
+            array[k] = righthalf[j]
+            j += 1
+            k += 1
+    return array
