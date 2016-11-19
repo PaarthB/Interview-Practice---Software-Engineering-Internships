6dfb0935accf1fdeaa5b94772c1dc1f47f803ef2
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-19 12:15:17 +1100

    Update Advanced Question.py

--- Arrays/Advanced Question.py
+++ Arrays/Advanced Question.py
@@ -25,47 +25,6 @@ Function: find_max_sum_interval(a_list):
 
     Best Case Complexity: O(N)
     Worst Case Complexity: O(N^2)
-
-Function: average_time_find_max_interval()
-
-    Preconditions: None
-    Postconditions: We get the average time taken to find max sum interval in a list of N elements
-                    (N = 2,4,8,16....1024).
-    TEST CASES:
-
-        VALID TEST CASE: all
-            a_list: A list of real numbers between 0 and 1.
-
-            Output:
-                n: 32
-                Average Time: 0.00013000965118408202
-
-                n: 64
-                Average Time: 0.0008400797843933106
-
-        INVALID TEST CASE: None
-
-    Best Case Complexity: O(N)
-    Worst Case Complexity: O(N^2)
-
-Function: time_find_max_interval(a_list)
-
-    VALID TEST CASE: When a_list contains only numbers
-        Inputs: [1,2,3,4]
-
-        Ouput: Time taken for finding max interval: 0.0
-
-    INVALID TEST CASE: When list doesn't contain only numbers
-        Inputs: ['b',1,4,3]
-
-        Output: unsupported operand type(s) for +=: 'int' and 'str'
-
-    Best Case Complexity: O(N)
-    Worst Case Complexity: O(N^2)
-    N: length of a_list
-
-
-
 '''
 
 import random
@@ -112,30 +71,6 @@ def find_max_sum_interval(a_list):
     print('Sliced List:', a)
     print('Start:', min)
     print('End:', max)
-
-
-def average_time_find_max_interval():
-    for j in range(1, 11):
-        count = 0
-        n = 2 ** j
-        time = 0
-        while count < 10:
-            a = []
-            for i in range(n):
-                a.append(random.random())
-                time += time_find_max_interval(a)
-            count += 1
-        print('n:', n)
-        average_time = time / 100
-        print('Average Time:', average_time)
-
-
-def time_find_max_interval(a_list):
-    start = time.time()
-    find_max_sum_interval(a_list)
-    taken = (time.time() - start)
-    return taken
-
 
 a = [1, 2, 3, 4]
 
