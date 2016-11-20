59b772959e3fe638fe3e70d26d229b8007e705d4
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-20 22:59:05 +1100

    Update anagrams.c

--- Strings/anagrams.c
+++ Strings/anagrams.c
@@ -10,8 +10,12 @@ void areAnagrams1(char *str1, char *str2){
 	
 }
 
-void areAnagrams2(char *str1, char *str2){
+int areAnagrams2(char *str1, char *str2){
 	// Using the method of comparing frequency
+	size_t len1 = strlen(str1);
+	size_t len2 = strlen(str2);
+	if(len1 != len2){
+		return 1;
 	char *arr;
 	arr = (char *)malloc(sizeof(char)*256);  // allocate 256 chars
 	for (int i=0; i<256; i++) // Setting all the ASCII char count values to 0.
@@ -19,7 +23,8 @@ void areAnagrams2(char *str1, char *str2){
 	  arr[i] = 0;
 	}
 	free(arr);
-
+	
+	return 0;
 }
 
 /**
