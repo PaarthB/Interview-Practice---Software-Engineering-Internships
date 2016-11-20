a5c9113702b9f5e0618b164731deff9db7e6c9d4
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-20 20:27:00 +1100

    Update anagrams.c

--- Strings/anagrams.c
+++ Strings/anagrams.c
@@ -12,7 +12,14 @@ void areAnagrams1(char *str1, char *str2){
 
 void areAnagrams2(char *str1, char *str2){
 	# Using the method of comparing frequency
+	array1 = (char *)malloc(sizeof(char)*256);  // allocate 256 chars
+	array2 = (char *)malloc(sizeof(char)*256);  // allocate 256 chars
 	
+	
+	
+	free(array1);
+	free(array2);
+
 }
 
 /**
