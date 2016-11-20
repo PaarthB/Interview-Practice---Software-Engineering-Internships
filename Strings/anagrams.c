6f7017626d6468b204a41565317bfe46ab07fcc6
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-20 22:43:11 +1100

    Update anagrams.c

--- Strings/anagrams.c
+++ Strings/anagrams.c
@@ -6,27 +6,25 @@
 #include <sys/time.h>
 
 void areAnagrams1(char *str1, char *str2){
-	# Using the method of sorting
+	// Using the method of sorting
 	
 }
 
 void areAnagrams2(char *str1, char *str2){
-	# Using the method of comparing frequency
-	char *array1;
-	char *array2;
-	array1 = (char *)malloc(sizeof(char)*256);  // allocate 256 chars
-	array2 = (char *)malloc(sizeof(char)*256);  // allocate 256 chars
-	
-	
-	
-	free(array1);
-	free(array2);
+	// Using the method of comparing frequency
+	char *arr;
+	arr = (char *)malloc(sizeof(char)*256);  // allocate 256 chars
+	for (int i=0; i<256; i++) // Setting all the ASCII char count values to 0.
+	{
+	  arr[i] = 0;
+	}
+	free(arr);
 
 }
 
 /**
 void areAnagrams3(char *str1, char *str2){
-	# Using the method of hash map.
+	// Using the method of hash map.
 	
 }
 **/
