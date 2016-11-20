1f6db460376c2165bd07520e4323f27c96cd5905
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-20 18:33:21 +1100

    Add files via upload

---
+++ Strings/anagrams.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/time.h>
+
+void areAnagrams1(char *str1, char *str2){
+	# Using the method of sorting
+	
+}
+
+void areAnagrams2(char *str1, char *str2){
+	# Using the method of comparing frequency
+	
+}
+
+void areAnagrams3(char *str1, char *str2){
+	# Using the method of hash map.
+	
+}
+
+int main(int argc, char* argv[]){
+	
+	
+	return 0;
+}
