413d741b4f29b2395a668ff3c0c48886df4ace62
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-24 14:59:48 +1100

    Update anagrams.c

--- Strings/anagrams.c
+++ Strings/anagrams.c
@@ -61,32 +61,36 @@ int areAnagrams2(char *str1, char *str2){
 // Quicksort implementation in C.
 void QuickSort(char *str1, int from, int to){
 	int index;
+	
 	if (from < to){
 	index = partition(str1, from, to);
 	QuickSort(str1, from, index-1);
 	QuickSort(str1, index+1, to);
 	}
-	
-	
 }
 	
 // Partition function for implementing QuickSort.
 int partition(char *str1, int p, int r){
 	int x = str1[p];
 	int i = p-1;
+	
 	for (int j = p; j < r; j++){
 	    if (str1[j] <= x){
 	    	i += 1;
 		swap(str, i, j);
 	    }
-		
 	}
-	
-	swap(str, i+1, r);
+	swap(str+(i+1), str+r);
 	return i+1;
 }
 
-
+void swap(char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
 int main(int argc, char* argv[]){
 	
 	int choice, result;
