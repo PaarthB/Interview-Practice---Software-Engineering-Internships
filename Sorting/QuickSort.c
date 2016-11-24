f869c83f4695a57482f420d3dca1876ae483180e
Paarth Bhasin <paarthbhasin4@gmail.com>
2016-11-24 15:44:32 +1100

    Update QuickSort.c

--- Sorting/QuickSort.c
+++ Sorting/QuickSort.c
@@ -10,25 +10,25 @@ void QuickSort(char *str1, int from, int to){
 	int index;
 	
 	if (from < to){
-	index = partition(str1, from, to);
-	QuickSort(str1, from, index-1);
-	QuickSort(str1, index+1, to);
+	    index = partition(str1, from, to);
+	    QuickSort(str1, from, index-1);
+	    QuickSort(str1, index+1, to);
 	}
 }
 	
 // Partition function for implementing QuickSort.
 int partition(char *str1, int p, int r){
 	int *x = str1+p;
-	int *i = p-1;
+	int i = p-1;
 	
 	for (int j = p; j < r; j++){
 	    if (&(str1+j) <= &x){
-	    	&i += 1;
+	    	i += 1;
 		swap(str, i, j);
 	    }
 	}
 	swap(str+(i+1), str+r);
-	return &(i+1);
+	return (i+1);
 }
 
 void swap(char *x, char *y)
