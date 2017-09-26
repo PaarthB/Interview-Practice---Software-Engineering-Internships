8dc00a8f57473a0563a2d99b8b94d973fcf35c5f
Paarth Bhasin <paarthbhasin4@gmail.com>
2017-09-27 00:01:21 +1000

    Add files via upload

--- hard questions.c
+++ hard questions.c
@@ -82,6 +82,35 @@ void _printParenthesis(int pos, int n, int open, int close)
     }
 }
 
+%%%%%%%%%%%%%%%%%%% REVERSE LINKED LIST RECURSIVE %%%%%%%%%%%%%%%%%%%%%%%
+
+void recursiveReverse(struct Node** head_ref)
+{
+    struct Node* first;
+    struct Node* rest;
+      
+    /* empty list */
+    if (*head_ref == NULL)
+       return;   
+ 
+    /* suppose first = {1, 2, 3}, rest = {2, 3} */
+    first = *head_ref;  
+    rest  = first->next;
+ 
+    /* List has only one node */
+    if (rest == NULL)
+       return;   
+ 
+    /* reverse the rest list and put the first element at the end */
+    recursiveReverse(&rest);
+    first->next->next  = first;  
+     
+    /* tricky step -- see the diagram */
+    first->next  = NULL;          
+ 
+    /* fix the head pointer */
+    *head_ref = rest;              
+}
 
 %%%%%%%%%%%%%%%%%%% IS SUBTREE %%%%%%%%%%%%%%%%%%%%%%%
 
