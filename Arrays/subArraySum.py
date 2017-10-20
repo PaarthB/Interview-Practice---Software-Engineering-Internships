16b0adc2301a5d6fb83a96587a892dce4a84119f
Paarth Bhasin <paarthbhasin4@gmail.com>
2017-10-21 09:15:20 +1100

    Add files via upload

---
+++ Arrays/subArraySum.py
@@ -0,0 +1,25 @@
+
+findSumInterval(array, sum):
+   n = len(array)
+   first = 0
+   last = 0
+   i = 0
+   j = 0
+   sum = array[0]
+   sum_curr = sum
+   if sum_curr == value:
+     return True
+
+   for j in range(i+1, n):
+     sum_curr += array[j]
+     while sum_curr > value:
+       sum_curr -= array[first]
+       first += 1
+
+     elif sum_curr < value:
+       continue
+     
+     elif sum_curr == value:
+       return True
+   
+   return False
