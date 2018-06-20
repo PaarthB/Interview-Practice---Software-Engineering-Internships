1ff654f3fa2b6a2be72cfe628387be3dec5896f8
Paarth Bhasin <paarthbhasin4@gmail.com>
2018-06-20 22:23:15 +1000

    Update replaceBinaryStringWildCard.py

--- General/replaceBinaryStringWildCard.py
+++ General/replaceBinaryStringWildCard.py
@@ -5,15 +5,14 @@ For example, for input 10?011, it returns ['101011', '100011']
 def returnPermutations(inputString):
     count = 0
     permutations = []
-    index = 0
     position = []
-    for i in inputString:
-        if i == "?":
-            position.append(index)
+    
+    for i in range(len(inputString)):
+        if inputString[i] == "?":
+            position.append(i)
             count += 1
-        index += 1
-    end = 1 << count
-    for j in range(0, end):
+            
+    for j in range(0, 1 << count):
         temp = j  # 0, 1
         listM = list(inputString)
         for k in position:  # [1]
@@ -21,5 +20,5 @@ def returnPermutations(inputString):
             listM[k] = str(bit)  # 000, 010
             temp >>= 1
         permutations.append(''.join(listM))
-
+        
     return permutations
